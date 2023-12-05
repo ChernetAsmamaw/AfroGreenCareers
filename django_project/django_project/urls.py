7879d2af9dc6317f13eb38abecea430319e9df34
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-12-05 12:37:45 +0300

    update

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.conf.urls import url 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +30,13 @@ urlpatterns = [
     path('job/', include('job.urls')),
     path('', include('website.urls'))
 ]
+
+url_patterns=[
+    ...
+
+    url(r'^favicon\.ico$',RedirectView.as_view(url='./media/favicon.ico')),
+]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
