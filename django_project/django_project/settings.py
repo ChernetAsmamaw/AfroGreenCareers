41cd4ab6be655a5d665e0b794af7d52d544a54ce
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-12-06 15:04:29 +0300

    changes to settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@ovn70(0cgoa7v-vg#$rawmgu%ezuz!#p)$co&uc^3akn&gbhm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
